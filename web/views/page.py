f96982d0d7d03913fd06a7aa22ca2979fb50c2c1
Eric Bitria <eric.bitria@gmail.com>
2025-03-03 15:52:06 +0100

    Added sub folders for song, playlist and reorganized base.html

--- web/views/page.py
+++ web/views/page.py
@@ -7,6 +7,14 @@ def home(request):
 def dashboard(request):
     return render(request, "dashboard.html")
 
+def dashboard_song(request):
+    # Render the same dashboard template but with song-specific content
+    return render(request, 'dashboard.html', {'active_page': 'song'})
+
+def dashboard_playlist(request):
+    # Render the same dashboard template but with song-specific content
+    return render(request, 'dashboard.html', {'active_page': 'playlist'})
+
 def logout(request):
     # Execute logout function
     spotify_logout(request)
